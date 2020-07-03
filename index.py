9a901687bace1f47d61d21032f658ca582c973d1
BrayanReyes <ing.brayan.reyes@gmail.com>
2020-07-03 15:36:12 -0500

    [ADD] Base Dashboard

---
+++ index.py
@@ -0,0 +1,125 @@
+# Basics Requirements
+import pathlib
+import dash
+from dash.dependencies import Input, Output, State, ClientsideFunction
+from dash.exceptions import PreventUpdate
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+import plotly.express as px
+
+# Dash Bootstrap Components
+import dash_bootstrap_components as dbc 
+
+#Data 
+import math
+import numpy as np
+import datetime as dt
+import pandas as pd
+import json
+
+#Recall app
+from app import app
+from lib import colombia_map,rips_charts
+
+# Build Functions
+
+def build_title():
+    return html.Div(
+        id="banner",
+        className="banner",
+        children=[
+            html.Div(
+                id="banner-text",
+                children=[
+                    html.H5("Analysis of the Health Service’s Demand with a Differential Approach"),
+                    html.H6("Exploratory Data Analysis and Prediction Model."),
+                ],
+            ),
+            html.Div(
+                id="banner-logo",
+                children=[
+                    #html.Button(
+                    #    id="learn-more-button", children="Team 61", n_clicks=0
+                    #),
+                    html.Img(id="logo", src=app.get_asset_url("DS4A_latam_logo.png")),
+                ],
+            ),
+        ],
+    )
+    
+def build_tabs():
+    return html.Div(
+        id="tabs",
+        className="tabs",
+        children=[
+            dcc.Tabs(
+                id="app-tabs",
+                value="tab1",
+                className="custom-tabs",
+                children=[
+                    dcc.Tab(
+                        id="Dashboard-tab",
+                        label="Main Dashboard",
+                        value="tab1",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                    ),
+                    dcc.Tab(
+                        id="Model-tab",
+                        label="Predition Model",
+                        value="tab2",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                    ),
+                ],
+            )
+        ],
+    )
+
+def build_tab_1():
+    return [
+        rips_charts.body,
+        dbc.Row([
+            dbc.Col([
+                html.Div(rips_charts.chart_col()),
+            ],id="quick-stats"),
+            dbc.Col(
+                dcc.Graph(figure=colombia_map.Map_Fig , id='Colombia_map'), id = "ccc"
+            ),
+        ])
+    ]
+
+def build_tab_2():
+    return [html.Div(html.H1("Under construction, excuse us. ")) ]
+
+
+#Create Layout
+
+app.layout = html.Div(
+    id="big-app-container",
+    children=[
+    build_title(),
+    html.Div(
+            id="app-container",
+            children=[
+                build_tabs(),
+                # Main app
+                html.Div(id="app-content"),
+            ],
+        )
+    ],
+)
+
+@app.callback(
+    Output("app-content", "children"),
+    [Input("app-tabs", "value")],
+)
+def render_tab_content(tab_switch):
+    if tab_switch == "tab1":
+        return build_tab_1()
+    else:
+        return build_tab_2()
+    
+if __name__ == "__main__":
+    app.run_server(debug=True)
